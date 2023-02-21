6f52e5d09beb0a981e01a9476c275c695eb52ff6
Cieran Sung <clsung154@gmail.com>
2023-02-21 01:43:08 +0000

    week5 complete

--- week5.hs
+++ week5.hs
@@ -3,6 +3,7 @@
 -}
 
 import Prelude hiding (fst, snd, head, tail, sum, concat, reverse, zip)
+import GHC.Exts.Heap (GenClosure(stack_marking))
 
 -- Definitions of the prelude functions fst and snd
 
@@ -95,9 +96,56 @@ countIntegers :: Int -> [Int] -> Int
 --     | otherwise = countIntegers x ys
 countIntegers x ys = sum [1 | y <- ys, x == y]
 
--- removeAll :: Int -> [Int] -> [Int]
--- removeAll _ [] = []
--- removeAll x (y:ys)
---     | x == y = removeAll x ys
---     | otherwise = removeAll x ys
+removeAll :: Int -> [Int] -> [Int]
+removeAll _ [] = []
+removeAll x (y:ys)
+    | x == y = removeAll x ys
+    | otherwise = y : removeAll x ys 
+
+removeAllButFirst :: Int -> [Int] -> [Int]
+removeAllButFirst x (y:ys)
+    | x == y = y : removeAll x ys
+    | otherwise = y : removeAllButFirst x ys
+
+type StudentMark = (String, Int)
+testData :: [StudentMark]
+testData =
+    [ 
+        ("John", 53),
+        ("Sam", 16),
+        ("Kate", 85),
+        ("Jill", 65),
+        ("Bill", 37),
+        ("Amy", 22),
+        ("Jack", 41),
+        ("Sue", 71)
+    ]
+
+listMarks :: String -> [StudentMark] -> [Int]
+-- listMarks _ [] = []
+-- listMarks str (x:xs) 
+--     | fst x == str = snd x : listMarks str xs
+--     | otherwise = listMarks str xs
+listMarks str stmks = [mk | (st, mk) <- stmks, st == str] 
+
+sorted :: [Int] -> Bool
+sorted [] = True
+sorted [x] = True
+sorted (x:y:xs) = x <= y && sorted (y:xs)
+
+prefix :: [Int] -> [Int] -> Bool
+prefix [] [] = True
+prefix [] _ = True
+prefix (x:xs) (y:ys)
+    | x == y = prefix xs ys
+    | otherwise = False
+
+subSequence :: [Int] -> [Int] -> Bool
+subSequence (x:xs) (y:ys)
+    | x == y = prefix xs ys
+    | otherwise = subSequence (x:xs) ys
+
+
+
+
 
